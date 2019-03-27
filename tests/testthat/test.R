5414918315426871faabbd7f3660eeab5981e705
JEFworks <jeanfan@fas.harvard.edu>
2019-03-26 21:59:39 -0400

    add codecov badge

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -8,28 +8,29 @@ test_that(context("Test GSEA"), {
   vals[gs] <- rnorm(length(gs), 100, 10)  
   gs.list <- org.Hs.GO2Symbol.list # get gene sets
   # test obviously enriched set, reduce n.rand for speed
-  pv <- gsea(values=vals, geneset=gs, mc.cores=1, n.rand=100, main="GO:Random") 
+  pv <- gsea(values=vals, geneset=gs, mc.cores=1, n.rand=100, plot=FALSE) 
   expect_equal(pv < 0.05, TRUE)
 })
 
 test_that(context("Test speed"), {
- data("org.Hs.GO2Symbol.list")  
- universe <- unique(unlist(org.Hs.GO2Symbol.list))  # get universe
- gs <- org.Hs.GO2Symbol.list[[1]]  # get a gene set
- vals <- rnorm(length(universe), 0, 10)  # simulate values
- names(vals) <- universe
- vals[gs] <- rnorm(length(gs), 100, 10)  
- gs.list <- org.Hs.GO2Symbol.list # get gene sets
+  library(liger)
+  data("org.Hs.GO2Symbol.list")  
+  universe <- unique(unlist(org.Hs.GO2Symbol.list))  # get universe
+  gs <- org.Hs.GO2Symbol.list[[1]]  # get a gene set
+  vals <- rnorm(length(universe), 0, 10)  # simulate values
+  names(vals) <- universe
+  vals[gs] <- rnorm(length(gs), 100, 10)  
+  gs.list <- org.Hs.GO2Symbol.list # get gene sets
  
- start_time <- Sys.time()
- bulk.gsea(values = vals, set.list = gs.list[1:3], mc.cores = 1, n.rand=100)
- end_time <- Sys.time()
- t1 <- end_time - start_time
+  start_time <- Sys.time()
+  bulk.gsea(values = vals, set.list = gs.list[1:3], mc.cores = 1, n.rand=100)
+  end_time <- Sys.time()
+  t1 <- end_time - start_time
  
- start_time <- Sys.time()
- iterative.bulk.gsea(values = vals, set.list = gs.list[1:3], mc.cores = 1, n.rand=100) 
- end_time <- Sys.time()
- t2 <- end_time - start_time
+  start_time <- Sys.time()
+  iterative.bulk.gsea(values = vals, set.list = gs.list[1:3], mc.cores = 1, n.rand=100) 
+  end_time <- Sys.time()
+  t2 <- end_time - start_time
  
- expect_equal(t2 <= t1, TRUE)
+  expect_equal(t2 <= t1, TRUE)
 })
